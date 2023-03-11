eec96e4b93c71b8a8054b7dd1ef4bf16f5899f96
意味がない <66781460+Nenind@users.noreply.github.com>
2023-03-12 00:36:07 +0500

    script for creating daemon hotfix

--- createDaemon.sh
+++ createDaemon.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-touch /etc/systemd/system/$(pwd).service
+name=$(basename "$PWD")
+touch /etc/systemd/system/$name.service
 
-cat > /etc/systemd/system/$(pwd).service <<EOF 
+cat > /etc/systemd/system/$name.service <<EOF 
 [Unit]
 Description="Best bot 4ever!"
 
+
 [Service]
-User=ubuntu
-WorkingDirectory=/home/ubuntu/$(pwd)
-VIRTUAL_ENV=/home/ubuntu/$(pwd)/venv
+User=root
+WorkingDirectory=$(pwd)
+VIRTUAL_ENV=$(pwd)/venv
 Environment=PATH=$VIRTUAL_ENV/bin:$PATH
-ExecStart=/home/ubuntu/$(pwd)/venv/bin/python main.py
+ExecStart=$(pwd)/venv/bin/python main.py
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-
-python3 -m venv venv
 python3 -m venv venv
 ./venv/bin/python -m pip install --upgrade pip
 ./venv/bin/pip install -r requirements.txt
 
 systemctl daemon-reload
-systemctl start $(pwd).service
-systemctl enable $(pwd).service
-systemctl status $(pwd).service
+systemctl start $name.service
+systemctl enable $name.service
+systemctl status $name.service
